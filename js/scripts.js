c1d53849c6bc5076a4dd6930d597742cd10efc5c
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2021-11-19 12:34:10 +0000

    coding update

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 
 //My array of pokemons, showing different data
-let pokemonList =  [
-{ name:'Blastoise', height: 5.03, type: ['Torrent']},
-{ name:'Venusaur', height:6.07, type: ['Overgrow']},
-{ name:'Charizard', height:5.07, type: ['Blaze']}
-];
+var pokemonList = [
+{
+  name:"Blastoise",
+  height: 5.03,
+  type:"Torrent"
+},
+{
+  name:"Venusaur",
+  height:6.07,
+  type:"Overgrow"
+},
+{
+  name:"Charizard",
+  height:5.07,
+  type:"Blaze"
+}
+]
+console.log (pokemonList[0].name)
