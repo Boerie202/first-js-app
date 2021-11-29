d1fd5dcbcb84538c85951d66af70961a46c21603
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2021-11-29 22:24:56 +0000

    Updated with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-
 //My array of pokemons, showing different data
-var pokemonList = [
+let pokemonRepository = (function(){
+let pokemonList = [
 {
   name:"Blastoise",
   height: 5.03,
@@ -15,8 +15,20 @@ var pokemonList = [
   name:"Charizard",
   height:5.07,
   type:"Blaze"
-}
+},
 ]
+
+//returns the pokemonList array
+function getAll (){
+return pokemonList;
+}
+
+//adds new data to the pokemonList array
+function add() {
+    pokemonList.push(pokemon);
+}
+)();
+
 pokemonList.forEach(function(pokemon){
 document.write(pokemon.name + "<br>");
 });
